79bbbd59608da072a31d2962c551f4c7548798d7
Dogu_ <dogu_rog@hotmail.com>
2025-05-15 11:21:58 +0300

    startup config

---
+++ AuthenticationApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,41 @@
+﻿using AuthenticationApi.Application.Interfaces;
+using AuthenticationApi.Infrastructure.Data;
+using AuthenticationApi.Infrastructure.Repositories;
+using eCommerceSharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            //add database connectivity
+            //add authentication scheme
+            SharedServiceContainer.AddSharedServices<AuthenticationDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            //Create dependency injection
+            services.AddScoped<IUser, UserRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UserInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //register missleware such as:
+            //global exception : handle external errors
+            //listen only to Api Gateway : block all outsiders calls
+            SharedServiceContainer.UseSharedPolicies(app);
+            return app;
+        }
+
+    }
+}
